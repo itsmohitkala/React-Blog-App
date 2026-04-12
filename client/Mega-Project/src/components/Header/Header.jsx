500d16d94b2815822a69e03285dc51570e13b9fd
upwork-fullstack-developer <inboxtocontact@gmail.com>
2026-04-12 22:46:25 +0530

    ausual comit X2

--- client/Mega-Project/src/components/Header/Header.jsx
+++ client/Mega-Project/src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Header() {
 
     const [loading, setLoading] = useState(true)
     const userData= useDispatch()
   return (
-  
+  useEffect(()=>{
+    
+  },[])
   )
+  !loading ? "" : "";
 }
 
 export default Header
