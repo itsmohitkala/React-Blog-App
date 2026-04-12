6b2fe6fec53d75885f794175aa77f2514474adb1
upwork-fullstack-developer <inboxtocontact@gmail.com>
2026-04-12 20:36:08 +0530

    Setup: header & Footer

--- client/Mega-Project/src/appwrite/Auth.js
+++ client/Mega-Project/src/appwrite/Auth.js
@@ -29,7 +29,7 @@ class AuthService{
         }
     }
 
-    async deleteSession(){
+    async logOut(){
         try{
             return await this.Account.deleteSessions();
         }catch(error){
