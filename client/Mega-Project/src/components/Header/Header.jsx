d799eff24c09b087ca70d1857aedaebfd63fab22
upwork-fullstack-developer <inboxtocontact@gmail.com>
2026-04-12 22:44:20 +0530

    Usual commit

--- client/Mega-Project/src/components/Header/Header.jsx
+++ client/Mega-Project/src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 
 function Header() {
+
+    const [loading, setLoading] = useState(true)
+    const userData= useDispatch()
   return (
-    <div>Header</div>
+  
   )
 }
 
