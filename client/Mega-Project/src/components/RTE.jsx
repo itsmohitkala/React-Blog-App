83d31734333cb0ce86cef725af99377c73d0b96b
Mohit Kala <itsmohitkala@gmail.com>
2026-05-18 22:24:12 +0530

    Setup: RTE & minor bugs fixes

---
+++ client/Mega-Project/src/components/RTE.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Editor} from '@tinymce/tinymce-react'
+function RTE() {
+  return (
+    <Editor
+    initialValue='this is demo bro '
+    />
+  )
+}
+
+export default RTE
