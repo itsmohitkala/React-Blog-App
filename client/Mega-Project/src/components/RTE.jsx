c94b1db6d614e5ca01e86ca4c753988a343fea9b
Mohit Kala <itsmohitkala@gmail.com>
2026-05-29 23:20:25 +0530

    rte

--- client/Mega-Project/src/components/RTE.jsx
+++ client/Mega-Project/src/components/RTE.jsx
@@ -1,52 +1,32 @@
 import React from 'react'
-import {Editor} from '@tinymce/tinymce-react'
-import {Controller} from 'react-hook-form'
-function RTE({name, control, defaultValue}) {
-  return (
-<div className='w-full '>
-  <Controller
-  name={name || "content"}
-  control={control}
-  defaultValue={defaultValue}
-  render={({field: {onChange}})=>(
-    <Editor
-      apiKey="YOUR_TINYMCE_API_KEY"
-      initialValue="<p>Start writing here...</p>"
-      init={{
-        height: 500,
-        menubar: true,
-        plugins: [
-          "advlist",
-          "autolink",
-          "lists",
-          "link",
-          "image",
-          "charmap",
-          "preview",
-          "anchor",
-          "searchreplace",
-          "visualblocks",
-          "code",
-          "fullscreen",
-          "insertdatetime",
-          "media",
-          "table",
-          "help",
-          "wordcount",
-        ],
-        toolbar:
-          "undo redo | blocks | " +
-          "bold italic forecolor | alignleft aligncenter " +
-          "alignright alignjustify | bullist numlist outdent indent | " +
-          "removeformat | help | code",
-        content_style:
-          "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-      }}
-    />
+import { Controller } from 'react-hook-form'
+import { Editor } from '@tinymce/tinymce-react';
+import {handleSubmit,register} from 'react-hook-form'
 
-  )}
-  />
-</div>
+
+function RTE({name,control,defaultValue=""}) {
+  
+  return (
+    <div>
+<Controller
+name={name  || "content"}
+defaultValue={defaultValue}
+control={control}
+
+render={({field: {onChange}})=>{
+render: <Editor
+name={name}
+initialValue='dmeo'
+init={{
+  height: 500,
+  menubar: false,
+
+  
+/>
+
+/>
+
+    </div>
   )
 }
 
