b333c38d060dd6d5706091f5c6113bf168cae859
Mohit Kala <itsmohitkala@gmail.com>
2026-08-12 10:22:12 +0530

    fixe: import

--- client/Mega-Project/src/components/PostForm.jsx
+++ client/Mega-Project/src/components/PostForm.jsx
@@ -7,8 +7,10 @@ import configure from '../appwrite/Configure'
 import config from '../config/Config'
 import {Input} from '../components/index'
 import Select from '../components/Header/Select'
+// import Select from '../components/Header/Select'
 // import Button from '../components/Header/Button'
-
+// All the neede imports
+import Container from '../components/index'
 
 
 function PostForm({post}) {
